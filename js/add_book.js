bea0e726ddf94d6b33b88562285183bab4f09064
Rajendra Prajapat <35336343+Rajpra786@users.noreply.github.com>
2019-05-17 20:32:25 +0530

    Update add_book.js

--- js/add_book.js
+++ js/add_book.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
     firebase.auth().onAuthStateChanged(user => {
         if(!user) {
-            window.location = 'main_page.html';
+            window.location = 'index.html';
             }
     });
 
